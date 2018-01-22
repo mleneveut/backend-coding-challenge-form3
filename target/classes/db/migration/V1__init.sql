8752253f5969e61d3fae385e142e86554a346b7c
mleneveut <mleneveut@gmail.com>
2018-01-22 12:19:48 +0100

    add gitignore

--- target/classes/db/migration/V1__init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE payment (
-  id UUID NOT NULL,
-  type VARCHAR(20) NOT NULL,
-  version INTEGER,
-  organisation_id UUID,
-  attributes json NOT NULL,
-
-  PRIMARY KEY (id)
-);
--- target/classes/schema.sql
+++
@@ -1,2 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS form3 AUTHORIZATION postgres;
-ALTER USER postgres SET search_path to form3;
